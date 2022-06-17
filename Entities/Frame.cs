86ffc5a9a148afad182e27ea6b3b458bfef907fa
suelio_notebook <josileissu@gmail.com>
2022-06-16 23:38:25 -0300

    Iniciado o objeto Frame (cópia do código da planilha do tcc em vba para c#)

---
+++ Entities/Frame.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fall.Entities
+{
+    internal class Frame
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int NodeI { get; set; } //tem que ser um objeto do tipo Nó
+        public int NodeF { get; set; } //Mesma coisa que o de cima;
+        public bool RotI { get; set; }
+        public bool RotF { get; set; }
+        public string Type { get; set; }
+        public double Length { get; set; }
+        public double Theta { get; set; } //angulo no plano xy;
+        public double Gamma { get; set; } //angulo no plano xy;
+        public string Section { get; set; } //objeto da seção - ver como atrelar um objeto;
+        //Tem que pegar as propriedades de área a1, a2, a3, E, G, I1, I2, I3, Peso,
+        public double QyLoad { get; set; }
+        public double QxLoad { get; set; }
+        public double PyLoad { get; set; }
+        public double PxLoad { get; set; }
+        public double MzLoad { get; set; }
+        public double DistPyLoad { get; set; }
+        public double DistPxLoad { get; set; } //fazer um array com as cargas Px consideradas e um array com as posições sendo correspondentes, por exemplo P1 na posição 1 e D1 na posição 1...
+        public double DistMzLoad { get; set; }
+        public double[][] RotateLocalToGlobal { get; set; }
+        public double[][] RotateGlobalToLocal { get; set; }
+        public double[][] FactorLoading { get; set; }
+        public double[][] Stiffness { get; set; }
+        public double[][] Forces { get; set; }
+        public double[][] Displacements { get; set; }
+        public Frame(int Id, string Name, double Gamma, int NodeI, int NodeF)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Gamma = Gamma;
+
+        }
+        public static double Atn2(double x, double y)
+        {
+            if (x == 0)
+            {
+                if (y == 0)
+                {
+                    return 9999999;
+                }
+                else if (y > 0)
+                {
+                    return Math.PI / 2;
+                }
+                else
+                {
+                    return -Math.PI / 2;
+                }
+            }
+            else if (x > 0)
+            {
+                if (y == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return Math.Atan(y / x);
+                }
+            }
+            else
+            {
+                if (y == 0)
+                {
+                    return Math.PI;
+                }
+                else
+                {
+                    return Math.PI - (Math.Sign(y) * Math.Atan(Math.Abs(y) / Math.Abs(x))
+                    }
+            }
+        }
+        }
+    }
--- Form_Main.Designer.cs
+++ Form_Main.Designer.cs
@@ -20,15 +20,17 @@ namespace Fall
             this.btn_Viga = new System.Windows.Forms.Button();
             this.btn_Pilares = new System.Windows.Forms.Button();
             this.btn_Fechar = new System.Windows.Forms.Button();
+            this.btn_Analise = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn_Viga
             // 
             this.btn_Viga.BackColor = System.Drawing.Color.LightCyan;
             this.btn_Viga.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btn_Viga.Location = new System.Drawing.Point(15, 15);
+            this.btn_Viga.Location = new System.Drawing.Point(17, 20);
+            this.btn_Viga.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn_Viga.Name = "btn_Viga";
-            this.btn_Viga.Size = new System.Drawing.Size(100, 50);
+            this.btn_Viga.Size = new System.Drawing.Size(114, 67);
             this.btn_Viga.TabIndex = 0;
             this.btn_Viga.Text = "Vigas";
             this.btn_Viga.UseVisualStyleBackColor = false;
@@ -38,9 +40,10 @@ namespace Fall
             // 
             this.btn_Pilares.BackColor = System.Drawing.Color.LightCyan;
             this.btn_Pilares.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btn_Pilares.Location = new System.Drawing.Point(121, 15);
+            this.btn_Pilares.Location = new System.Drawing.Point(138, 20);
+            this.btn_Pilares.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn_Pilares.Name = "btn_Pilares";
-            this.btn_Pilares.Size = new System.Drawing.Size(100, 50);
+            this.btn_Pilares.Size = new System.Drawing.Size(114, 67);
             this.btn_Pilares.TabIndex = 1;
             this.btn_Pilares.Text = "Pilares";
             this.btn_Pilares.UseVisualStyleBackColor = false;
@@ -50,25 +53,39 @@ namespace Fall
             // 
             this.btn_Fechar.BackColor = System.Drawing.Color.DimGray;
             this.btn_Fechar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btn_Fechar.Location = new System.Drawing.Point(888, 438);
+            this.btn_Fechar.Location = new System.Drawing.Point(1015, 584);
+            this.btn_Fechar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn_Fechar.Name = "btn_Fechar";
-            this.btn_Fechar.Size = new System.Drawing.Size(100, 50);
+            this.btn_Fechar.Size = new System.Drawing.Size(114, 67);
             this.btn_Fechar.TabIndex = 0;
             this.btn_Fechar.Text = "Fechar";
             this.btn_Fechar.UseVisualStyleBackColor = false;
             this.btn_Fechar.Click += new System.EventHandler(this.btn_Fechar_Click);
             // 
+            // btn_Analise
+            // 
+            this.btn_Analise.BackColor = System.Drawing.Color.LightCyan;
+            this.btn_Analise.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_Analise.Location = new System.Drawing.Point(258, 20);
+            this.btn_Analise.Name = "btn_Analise";
+            this.btn_Analise.Size = new System.Drawing.Size(114, 67);
+            this.btn_Analise.TabIndex = 2;
+            this.btn_Analise.Text = "Análise";
+            this.btn_Analise.UseVisualStyleBackColor = false;
+            // 
             // Form_Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.ClientSize = new System.Drawing.Size(1000, 500);
+            this.ClientSize = new System.Drawing.Size(1143, 667);
+            this.Controls.Add(this.btn_Analise);
             this.Controls.Add(this.btn_Pilares);
             this.Controls.Add(this.btn_Fechar);
             this.Controls.Add(this.btn_Viga);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form_Main";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -82,6 +99,7 @@ namespace Fall
         private System.Windows.Forms.Button btn_Viga;
         private System.Windows.Forms.Button btn_Pilares;
         private System.Windows.Forms.Button btn_Fechar;
+        private System.Windows.Forms.Button btn_Analise;
     }
 }
 
